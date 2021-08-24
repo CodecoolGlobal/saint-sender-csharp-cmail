824aaa3850443e0a636ce697fb8469ce5a0e77fd
agocsvince <64377515+agocsvince@users.noreply.github.com>
2021-08-24 13:18:01 +0200

    implement login authentication and services

---
+++ SaintSender.Core/Services/AccountService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SaintSender.Core.Interfaces;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MailKit;
+using MimeKit;
+using Org.BouncyCastle.Crypto.Generators;
+using SaintSender.Core.Models;
+
+namespace SaintSender.Core.Services
+{
+    public class AccountService : IAccountService
+    {
+        public string Authenticate(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+		}
+
+		public bool VerifyPassword(string password)
+		{
+			return BCrypt.Net.BCrypt.Verify(password, HashPassword(password));
+		}
+    }
+}
