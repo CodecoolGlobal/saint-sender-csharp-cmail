d36e60e8e0b354fcd8662e5f37e68e2074a73346
agocsvince <vince.agocs@gmail.com>
2021-08-26 12:53:47 +0200

    delete unused lines

--- SaintSender.DesktopUI/ViewModels/MainWindowViewModel.cs
+++ SaintSender.DesktopUI/ViewModels/MainWindowViewModel.cs
@@ -78,8 +78,6 @@ namespace SaintSender.DesktopUI.ViewModels
 
         public void Login(string name, string password)
         {
-            Thread thread = Thread.CurrentThread;
-            thread.Name = "Main";
             StatusCodes status = _accountService.Authenticate(name, password);
             Message = StatusCodeParser.GetStatusMessage(status);
             
